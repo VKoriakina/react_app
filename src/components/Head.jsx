cdffb813cf8c5bc8f96b43d392b35cb60acc070c
Valentyna.Koriakina <ysmiwko@gmail.com>
2024-07-30 12:41:05 +0200

    1 lesson react

---
+++ src/components/Head.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Head =(props)=> {
+    return <p className ={props.clas} id={props.id}>
+        I am Head
+    </p>
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {Card} from "./components/Card";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// import "./index.css"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// JS
+// const div = document.createElement('div');
+// const root = document.getElementById('root');
+// root.append(div);
+
+//REACT
+// const paragraph = React.createElement('p',{
+//     id: 'test-P-Id',
+//     children: ['I am P']
+// });
+
+// const p =<p id="test-ID"> I am P</p>
+
+// const div = React.createElement('div', {
+//     name:"main",
+//     id:"test-Id",
+//     className: "test-class",
+//     children: p,
+// });
+//
+// const div = (
+//         <div name='main' id='test Id' className = 'test-class'>
+//             {p}
+//         </div>
+//     )
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Card />);
+
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<React.StrictMode></React.StrictMode>);
